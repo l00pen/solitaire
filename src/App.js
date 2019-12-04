200ac7bc5630541a10a0118774579165c65ff082
l00pen <lovisa.skeppe@gmail.com>
2019-12-04 08:53:50 +0100

    Visually display all pile areas

--- src/App.js
+++ src/App.js
@@ -1,60 +1,94 @@
 import React, { useState } from 'react';
+
+import { deck } from './utils/';
+
 import './App.css';
 
 const cardBack = '🂠';
 
-const Card = ({ value, suite }) => {
+// const CardWaste
+// const CardStock
+// const CardFoundation
+
+const Card = ({ label, suite }) => {
   const [isVisible, setIsVisible] = useState(false);
   const toggleVisibility = () => {
     setIsVisible(!isVisible);
   }
 
   if (isVisible) {
-    return <div className={`Card Card-suite-${suite}`} onClick={toggleVisibility}>{value}</div>;
+    return <div className={`Card Card-suite-${suite}`} onClick={toggleVisibility}>{label}</div>;
   }
   return <div className={`Card`} onClick={toggleVisibility}>{cardBack}</div>;
 }
 
-const spades = {
-  id: 'spades',
-  suite: ['🂡','🂮','🂭','🂫','🂪','🂩','🂨','🂧','🂦','🂥','🂤','🂣','🂢'],
-};
-
-const hearts = {
-  id: 'hearts',
-  suite: ['🂱','🂾','🂽','🂻','🂺','🂹','🂸','🂷','🂶','🂵','🂴','🂳','🂲'],
-};
-
-const diamonds = {
-  id: 'diamonds',
-  suite: ['🃁','🃎','🃍','🃋','🃊','🃉','🃈','🃇','🃆','🃅','🃄','🃃','🃂'],
-};
-
-const clubs = {
-  id: 'clubs',
-  suite: ['🃑', '🃞', '🃝', '🃛', '🃚', '🃙', '🃘', '🃗', '🃖', '🃕', '🃔', '🃓', '🃒'],
-};
-
-
-const deck = [spades, hearts, diamonds, clubs];
+const game = {
+  foundation: [
+    [],
+    [],
+    [],
+    [],
+  ],
+  tableau: [
+    deck.slice(0, 1),
+    deck.slice(1, 3),
+    deck.slice(3, 6),
+    deck.slice(6, 10),
+    deck.slice(10, 15),
+    deck.slice(15, 21),
+    deck.slice(21, 28),
+  ],
+  stock: deck.slice(28),
+  waste: [],
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        {deck.map(({id, suite}) => (
-          <ul key={id} className={`App-suite`}>
-            {suite.map((card) => (
-              <li className='App-card' key={card}>
-                <Card
-                  value={card}
-                  suite={id}
-                />
+    <div className="App App-header">
+      <section className='Game-top'>
+        <section className='Foundation'>
+          {game.foundation.map((pile) => (
+            <ul className={`Foundation-pile ${pile.length === 0 ? 'empty' : ''}`}>
+              {pile.map((card) => (
+                <li className='App-card' key={card.value}>
+                  <Card {...card} />
+                </li>
+              ))}
+            </ul>
+          ))}
+        </section>
+        <div className='Game-stockAndWaste'>
+          <section className='Waste'>
+            <ul className={`Waste-pile ${game.waste.length === 0 ? 'empty' : ''}`}>
+              {game.waste.map((card) => (
+                <li className='App-card Waste-card' key={card.value}>
+                  <Card {...card} />
+                </li>
+              ))}
+            </ul>
+          </section>
+          <section className='Stock'>
+            <ul className={`Stock-pile ${game.stock.length === 0 ? 'empty' : ''}`}>
+              {game.stock.map((card) => (
+                <li className='App-card Stock-card' key={card.value}>
+                  <Card {...card} />
+                </li>
+              ))}
+            </ul>
+          </section>
+        </div>
+      </section>
+      <section className='Tableau'>
+        {game.tableau.map((pile) => (
+          <ul className={`Tableau-pile ${pile.length === 0 ? 'empty' : ''}`}>
+            {pile.map((card) => (
+              <li className='App-card' key={card.value}>
+                <Card {...card} />
               </li>
             ))}
           </ul>
         ))}
-      </header>
+      </section>
     </div>
   );
 }
---
+++ src/utils/index.js
@@ -0,0 +1,121 @@
+const shuffleArray = (array) => {
+  var currentIndex = array.length, temporaryValue, randomIndex;
+
+  // While there remain elements to shuffle...
+  while (0 !== currentIndex) {
+
+    // Pick a remaining element...
+    randomIndex = Math.floor(Math.random() * currentIndex);
+    currentIndex -= 1;
+
+    // And swap it with the current element.
+    temporaryValue = array[currentIndex];
+    array[currentIndex] = array[randomIndex];
+    array[randomIndex] = temporaryValue;
+  }
+
+  return array;
+}
+
+// const spades = {
+//   id: 'spades',
+//   suite: shuffleArray(['🂡','🂮','🂭','🂫','🂪','🂩','🂨','🂧','🂦','🂥','🂤','🂣','🂢']),
+// };
+
+// const hearts = {
+//   id: 'hearts',
+//   suite: shuffleArray(['🂱','🂾','🂽','🂻','🂺','🂹','🂸','🂷','🂶','🂵','🂴','🂳','🂲']),
+// };
+
+// const diamonds = {
+//   id: 'diamonds',
+//   suite: shuffleArray(['🃁','🃎','🃍','🃋','🃊','🃉','🃈','🃇','🃆','🃅','🃄','🃃','🃂']),
+// };
+
+const clubs = shuffleArray([
+    { 
+      label: '🃑',
+      value: 1,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃞',
+      value: 13,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃝',
+      value: 12,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃛',
+      value: 11,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃚',
+      value: 10,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃙',
+      value: 9,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃘',
+      value: 8,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃗',
+      value: 7,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃖',
+      value: 6,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃕',
+      value: 5,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃔',
+      value: 4,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃓',
+      value: 3,
+      suite: 'clubs',
+      color: 'black',
+    },
+    { 
+      label: '🃒',
+      value: 2,
+      suite: 'clubs',
+      color: 'black',
+    }
+  ]);
+
+const deck = [clubs, clubs, clubs, clubs].flat();
+
+export {
+  shuffleArray,
+  deck,
+};
