6f17fd324222abfac981b4770539efda398a1387
l00pen <lovisa.skeppe@gmail.com>
2019-12-06 15:59:33 +0100

    Not working bad

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { DragDropContext, Droppable, Draggable } from 'react-beautiful-dnd';
 
 import { deck } from './utils/';
 import CardTableau from './CardTableau';
@@ -96,46 +97,168 @@ function App() {
     }
   }
 
+  const moveToPile = (card, type, id) => {
+    switch(type) {
+      case 'tableau':
+      case 'foundation':
+        const newTableau = [...game.tableau];
+        const newTableauPile = [...newTableau[id]];
+        newTableauPile.push(card);
+        newTableau[id] = newTableauPile;
+        return newTableau;
+      default:
+        const newPile = [...game[type]];
+        newPile.push(card);
+        return newPile;
+    }
+
+  }
+
+  const moveFromPile = (cardSourceIndex, pileType, pileId) => {
+    switch(pileType) {
+      case 'tableau':
+      case 'foundation':
+        const newTableau = [...game.tableau];
+        const newTableauPile = [...newTableau[pileId]];
+        newTableauPile.splice(cardSourceIndex);
+        newTableau[pileId] = newTableauPile;
+        return newTableau;
+      default:
+        const newPile = [...game[pileType]];
+        newPile.splice(cardSourceIndex);
+        return newPile;
+    }
+  }
+
+  const getCardFromSource = (cardSourceIndex, type, id) => {
+    switch(type) {
+      case 'tableau':
+      case 'foundation':
+        return game[type][id].findIndex(sC => sC.id === cardSourceIndex);
+      default:
+        return game[type].findIndex(sC => sC.id === cardSourceIndex);
+    }
+  }
+
+  const getCardIndexFromSource = (cardSourceIndex, type, id) => {
+    switch(type) {
+      case 'tableau':
+      case 'foundation':
+        return game[type][id].find(sC => sC.id === cardSourceIndex);
+      default:
+        return game[type].find(sC => sC.id === cardSourceIndex);
+    }
+  }
+
+  const onDragEnd = result => {
+    const { draggableId, source, destination } = result;
+
+    if (!destination) {
+      return;
+    }
+
+    if (source.droppableId === destination.droppableId) {
+      return false;
+    } else {
+      console.log(draggableId, source)
+      const [sourceType, sourceId] = source.droppableId.split('-');
+      const [destType, destId] = destination.droppableId.split('-');
+      const card = getCardFromSource(draggableId, sourceType, sourceId);
+      const cardIndex = getCardIndexFromSource(draggableId, sourceType, sourceId);
+      const newDestination = moveToPile(card, destType, destId);
+      const newSource = moveFromPile(cardIndex, sourceType, sourceId);
+      const tmp = {
+        ...game,
+        [sourceType]: newSource,
+        [destType]: newDestination,
+      }
+      setGame(tmp)
+    }
+  };
+
   return (
     <div className="App App-header">
-      <section className='Game-top'>
-        <section className='Foundation'>
-          {game.foundation.map((pile, pileIndex) => (
-            <ul className={`Foundation-pile ${getEmptyClass(pile)}`} key={`foundation-${pileIndex}`}>
-              {pile.map((card) => (
-                <li className='App-card' key={card.id}>
-                  <CardTableau {...card} />
-                </li>
-              ))}
+      <DragDropContext onDragEnd={onDragEnd}>
+        <section className='Game-top'>
+          <section className='Foundation'>
+            {game.foundation.map((pile, pileIndex) => (
+              <ul className={`Foundation-pile ${getEmptyClass(pile)}`} key={`foundation-${pileIndex}`}>
+                {pile.map((card) => (
+                  <li className='App-card' key={card.id}>
+                    <CardTableau {...card} />
+                  </li>
+                ))}
+              </ul>
+            ))}
+          </section>
+          <div className='Game-stockAndWaste'>
+            <section className='Waste'>
+              <Droppable droppableId="waste-0">
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+                    {...provided.droppableProps}
+                  >
+                    <PileWaste pile={game.waste} />
+                    {provided.placeholder}
+                  </div>
+                )}
+              </Droppable>
+            </section>
+            <section className='Stock'>
+              <ul className={`Stock-pile ${getEmptyClass(game.stock)}`}>
+                {game.stock.map((card, cardIndex) => (
+                  <li className='App-card Stock-card' key={card.id}>
+                    <CardFaceDown {...card} onClick={() => { stockPileCardClickHandler(cardIndex) }} />
+                  </li>
+                ))}
+              </ul>
+            </section>
+          </div>
+        </section>
+        <section className='Tableau'>
+          {game.tableau.map((pile, pileIndex) => (
+            <ul className={`Tableau-pile ${getEmptyClass(pile)}`} key={`tableau-${pileIndex}`}>
+              {pile.map((card, cardIndex) => {
+                if (cardIndex === pile.length - 1) {
+                  return (
+                    <li className='Tableau-card' key={card.id + pileIndex}>
+                      <Droppable droppableId={`tableau-${pileIndex}`}>
+                      {(provided, snapshot) => (
+                        <div
+                          ref={provided.innerRef}
+                          style={{ backgroundColor: snapshot.isDraggingOver ? 'blue' : 'grey' }}
+                          {...provided.droppableProps}
+                        >
+                          <Draggable draggableId={card.id} index={cardIndex}>
+                            {(provided, snapshot) => (
+                              <div
+                                ref={provided.innerRef}
+                                {...provided.draggableProps}
+                                {...provided.dragHandleProps}
+                              >
+                                <CardTableau {...card} onClick={() => { tableauCardClickHandler(card, cardIndex, pileIndex) } } />
+                              </div>
+                            )}
+                          </Draggable>
+                          {provided.placeholder}
+                        </div>
+                      )}
+                    </Droppable>
+                  </li>
+                  );
+                }
+                return (
+                  <li className='Tableau-card' key={card.id + pileIndex}>
+                    <CardTableau {...card} onClick={() => { tableauCardClickHandler(card, cardIndex, pileIndex) } } />
+                  </li>
+                )
+              })}
             </ul>
           ))}
         </section>
-        <div className='Game-stockAndWaste'>
-          <section className='Waste'>
-            <PileWaste pile={game.waste} />
-          </section>
-          <section className='Stock'>
-            <ul className={`Stock-pile ${getEmptyClass(game.stock)}`}>
-              {game.stock.map((card, cardIndex) => (
-                <li className='App-card Stock-card' key={card.id}>
-                  <CardFaceDown {...card} onClick={() => { stockPileCardClickHandler(cardIndex) }} />
-                </li>
-              ))}
-            </ul>
-          </section>
-        </div>
-      </section>
-      <section className='Tableau'>
-        {game.tableau.map((pile, pileIndex) => (
-          <ul className={`Tableau-pile ${getEmptyClass(pile)}`} key={`tableau-${pileIndex}`}>
-            {pile.map((card, cardIndex) => (
-              <li className='Tableau-card' key={card.id + pileIndex}>
-                <CardTableau {...card} onClick={() => { tableauCardClickHandler(card, cardIndex, pileIndex) } } />
-              </li>
-            ))}
-          </ul>
-        ))}
-      </section>
+      </DragDropContext>
     </div>
   );
 }
--- src/PileWaste/index.js
+++ src/PileWaste/index.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Draggable } from 'react-beautiful-dnd';
 
 import CardTableau from '../CardTableau';
 
@@ -11,7 +12,24 @@ const getEmptyClass = (list) => {
 const PileWaste = ({ pile }) => {
   return (
     <ul className={`Waste-pile ${getEmptyClass(pile)}`}>
-      {pile.map((card) => {
+      {pile.map((card, i) => {
+        if (i === pile.length - 1) {
+          return (
+            <li className='App-card Waste-card' key={card.id}>
+              <Draggable draggableId={card.id} index={i}>
+                {(provided, snapshot) => (
+                  <div
+                    ref={provided.innerRef}
+                    {...provided.draggableProps}
+                    {...provided.dragHandleProps}
+                  >
+                    <CardTableau {...card} />
+                  </div>
+                )}
+              </Draggable>
+            </li>
+          );
+        }
         return (
           <li className='App-card Waste-card' key={card.id}>
             <CardTableau {...card} />
