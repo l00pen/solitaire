6edce6ddf72608046649c6d7475c2a1f54b6ad48
l00pen <lovisa.skeppe@gmail.com>
2020-01-21 15:29:46 +0100

    Add card images

--- src/Card/index.js
+++ src/Card/index.js
@@ -2,17 +2,44 @@ import React from 'react';
 
 import './styles.css';
 
-const cardBack = '🂠';
+function importAll(r) {
+  let images = {};
+  r.keys().map((item, index) => ( images[item.replace('./', '')] = r(item) ));
+  return images;
+}
 
-const CardFaceUp = ({ label, suite, onClick, ...props }) => (
-  <div className={`Card Card-suite-${suite}`} onClick={onClick} {...props}>
-    {label}
-  </div>
-)
+const images = importAll(require.context('./images/cards', false, /\.(png|jpe?g|svg)$/));
+
+const CardFaceUp = ({ id, label, suite, onClick, ...props }) => { 
+  const newId = id.match(/[a-z]+|[^a-z]+/gi);
+  const idSuite = newId[0].toUpperCase();
+  let idValue = newId[1];
+  if (idValue === '1') {
+    idValue = 'A';
+  }
+
+  if (idValue === '13') {
+    idValue = 'K';
+  }
+
+  if (idValue === '12') {
+    idValue = 'Q';
+  }
+
+  if (idValue === '11') {
+    idValue = 'J';
+  }
+  console.log('hej', id, newId, idValue)
+
+  return(
+    <div className={`Card Card-suite-${suite}`} onClick={onClick} {...props}>
+      <img src={images[`${idValue}${idSuite}.png`]} alt='card background' />
+    </div>
+  )}
 
 const CardFaceDown = ({ onClick }) => (
   <div className={`Card`} onClick={() => onClick ? onClick() : null}>
-    {cardBack}
+    <img src={images['green_back.png']} alt='card background' />
   </div>
 );
 
