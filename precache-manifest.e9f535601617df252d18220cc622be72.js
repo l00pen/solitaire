f9d54d9e06e77788bf5668d69214e12414b14b58
l00pen <lovisa.skeppe@gmail.com>
2020-05-26 21:44:18 +0200

    Updates

---
+++ precache-manifest.e9f535601617df252d18220cc622be72.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "0fe4a8eeff676e8f549af5bfd1963f6b",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "f1cd72aefa85051244dc",
+    "url": "/solitaire/static/css/main.957be3b9.chunk.css"
+  },
+  {
+    "revision": "d0a3a0b47d29242a49f4",
+    "url": "/solitaire/static/js/2.f0620e77.chunk.js"
+  },
+  {
+    "revision": "f1cd72aefa85051244dc",
+    "url": "/solitaire/static/js/main.705d82bb.chunk.js"
+  },
+  {
+    "revision": "a647a0caccceb8d03464",
+    "url": "/solitaire/static/js/runtime-main.e080015c.js"
+  },
+  {
+    "revision": "cf1608c458a2b30c2601c39b7994426b",
+    "url": "/solitaire/static/media/lov.cf1608c4.svg"
+  }
+]);
