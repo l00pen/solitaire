0f17c08f27c8438202b72e6a8d29d8c3f5d23b1e
l00pen <lovisa.skeppe@gmail.com>
2020-09-13 13:11:55 +0200

    Updates

---
+++ precache-manifest.b91cb058ad443ecbd99064bdf3f281cd.js
@@ -0,0 +1,18 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5d2897428e0494af257df9386fdc400",
+    "url": "/solitaire/index.html"
+  },
+  {
+    "revision": "1cee01191d0529ba8750",
+    "url": "/solitaire/static/js/2.c52a274c.chunk.js"
+  },
+  {
+    "revision": "7ae6598af39f6e09de9c",
+    "url": "/solitaire/static/js/main.03d57118.chunk.js"
+  },
+  {
+    "revision": "a647a0caccceb8d03464",
+    "url": "/solitaire/static/js/runtime-main.e080015c.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/solitaire/precache-manifest.10fa373a8a6053ad5736281a99a387bc.js"
+  "/solitaire/precache-manifest.b91cb058ad443ecbd99064bdf3f281cd.js"
 );
 
 self.addEventListener('message', (event) => {
